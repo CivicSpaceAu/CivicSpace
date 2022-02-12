1729cb973964db657ef4dbaa3ee5969d15afddeb
Jase Banico <jasebanico@gmail.com>
2022-02-13 09:24:35 +1100

    using ReactJS.Net

---
+++ src/CivicSpace.Web.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CivicSpace.Web.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
