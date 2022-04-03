b1ff4d25c46c2594e1eb7f0b20cad76958b105fa
Jase Banico <jasebanico@gmail.com>
2022-04-03 19:18:27 +1000

    webpack

---
+++ src/CivicSpace.Client/ClientApp/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './wwwroot/js/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './wwwroot/js/dist'),
+        filename: 'bundle.js',
+        publicPath: 'dist/'
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    'presets': ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        }]
+    }
+};
