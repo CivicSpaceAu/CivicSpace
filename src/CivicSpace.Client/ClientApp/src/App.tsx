17026f5921048e6b4cecccb77280325c9870e8f0
Jase Banico <jasebanico@gmail.com>
2022-04-12 16:52:36 +1000

    dependency update

--- src/CivicSpace.Client/ClientApp/src/App.tsx
+++ src/CivicSpace.Client/ClientApp/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Counter } from './features/counter/counter';
 import './app.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Counter />
       </header>
     </div>
   );
