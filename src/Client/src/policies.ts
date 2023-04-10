417c6ea895d07eb7a84e4805e340dc441847e5cc
Jason Banico <jason.banico@recordpoint.com>
2023-04-11 08:31:54 +1000

    authorities url fix

--- src/Client/src/policies.ts
+++ src/Client/src/policies.ts
@@ -6,13 +6,13 @@ export const b2cPolicies = {
     },
     authorities: {
         signUpSignIn: {
-            authority: "https://civicspaceb2c.b2clogin.com/civicspace-web-app.onmicrosoft.com/B2C_1_susi",
+            authority: "https://civicspaceb2c.b2clogin.com/civicspaceb2c.onmicrosoft.com/B2C_1_susi",
         },
         forgotPassword: {
-            authority: "https://civicspaceb2c.b2clogin.com/civicspace-web-app.onmicrosoft.com/B2C_1_reset_password",
+            authority: "https://civicspaceb2c.b2clogin.com/civicspaceb2c.onmicrosoft.com/B2C_1_reset_password",
         },
         editProfile: {
-            authority: "https://civicspaceb2c.b2clogin.com/civicspace-web-app.onmicrosoft.com/B2C_1_edit_profile"
+            authority: "https://civicspaceb2c.b2clogin.com/civicspaceb2c.onmicrosoft.com/B2C_1_edit_profile"
         }
     },
     authorityDomain: "civicspaceb2c.b2clogin.com"
