287182b227e63b11d82017733b79d97574a505a1
Jason Banico <jason.banico@recordpoint.com>
2023-05-08 13:32:07 +1000

    moving to cosmos

--- src/Server/CivicSpace.Data/AppDbContext.cs
+++ src/Server/CivicSpace.Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using CivicSpace.Core.Content;
+using Microsoft.Azure.Cosmos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -6,58 +7,12 @@ namespace CivicSpace.Data
 {
     public class AppDbContext : DbContext
     {
-        private IConfigurationRoot _configuration;
-        private readonly bool _isMigration = false;
-        public string _connectionString = String.Empty;
+        private readonly IConfigurationRoot _configuration;
+        private readonly CosmosClient _cosmosClient;
 
         public AppDbContext(IConfigurationRoot configuration)
         {
-            _isMigration = true;
             _configuration = configuration;
-        }
-
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (_isMigration)
-            {
-                string connectionString = _configuration.GetConnectionString("AppDbContext");
-
-                // Override with Azure connection string if exists
-                var azureConnectionStringEnvironmentVariable = _configuration["AzureConnectionStringEnvironmentVariable"];
-                if (!string.IsNullOrEmpty(azureConnectionStringEnvironmentVariable))
-                {
-                    connectionString = Environment.GetEnvironmentVariable(azureConnectionStringEnvironmentVariable);
-                    //connectionString = AzureMySQL.ToMySQLStandard(connectionString);
-                }
-
-                var provider = _configuration["AppDbProvider"];
-                if (string.IsNullOrEmpty(provider))
-                {
-                    provider = "mssql";
-                }
-                switch(provider.ToLower())
-                {
-                    case "mssql":
-                        optionsBuilder.UseSqlServer(connectionString);
-                        break;
-                    //case "mysql":
-                    //    optionsBuilder.UseMySql(connectionString);
-                    //    break;
-                    //case "sqlite":
-                    //    if (string.IsNullOrEmpty(connectionString))
-                    //    {
-                    //        var connectionStringBuilder = new Microsoft.Data.Sqlite.SqliteConnectionStringBuilder { DataSource = "banico.db" };
-                    //        connectionString = connectionStringBuilder.ToString();
-                    //    }
-                    //    optionsBuilder.UseSqlite(connectionString);
-                    //    break;
-                }
-            }
         }
 
         public DbSet<Node> Nodes { get; set; }
@@ -66,5 +21,31 @@ namespace CivicSpace.Data
         public DbSet<NodeReaction> NodeReactions { get; set; }
         public DbSet<NodeTag> NodeTags { get; set; }
         public DbSet<NodeVote> NodeVotes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Node>().ToContainer("Nodes");
+            modelBuilder.Entity<NodeCustomField>().ToContainer("NodeCustomFields");
+            modelBuilder.Entity<NodeLink>().ToContainer("NodeLinks");
+            modelBuilder.Entity<NodeReaction>().ToContainer("NodeReactions");
+            modelBuilder.Entity<NodeTag>().ToContainer("NodeTags");
+            modelBuilder.Entity<NodeVote>().ToContainer("NodeVotes");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var cosmosDbConnectionString = _configuration.GetConnectionString("CosmosDbConnection");
+            var cosmosDbName = _configuration["CosmosDbDatabaseName"];
+            if (!string.IsNullOrWhiteSpace(cosmosDbConnectionString))
+            {
+                optionsBuilder.UseCosmos(cosmosDbConnectionString, cosmosDbName);
+            }
+            else
+            {
+                var cosmosDbEndpoint = _configuration["CosmosDbEndpoint"] ?? throw new ArgumentNullException("Configuration:CosmosDbEndpoint");
+                var cosmosDbKey = _configuration["CosmosDbKey"] ?? throw new ArgumentNullException("Configuration:CosmosDbKey");
+                optionsBuilder.UseCosmos(cosmosDbEndpoint, cosmosDbKey, cosmosDbName);
+            }
+        }
     }
 }
--- src/Server/CivicSpace.Data/DataStartup.cs
+++ src/Server/CivicSpace.Data/DataStartup.cs
@@ -10,49 +10,16 @@ namespace CivicSpace.Data
   {
     public void ConfigureServices(IConfiguration configuration, IServiceCollection services)
     {
-      string appDbContextConnectionString = configuration.GetConnectionString("AppDbContext");
-
-      // Override with Azure connection string if exists
-      var azureConnectionStringEnvironmentVariable = configuration["AzureConnectionStringEnvironmentVariable"];
-      if (!string.IsNullOrEmpty(azureConnectionStringEnvironmentVariable))
-      {
-          appDbContextConnectionString = Environment.GetEnvironmentVariable(azureConnectionStringEnvironmentVariable);
-      }
-
-      var provider = configuration["AppDbProvider"];
-      if (string.IsNullOrEmpty(provider))
-      {
-          provider = "mssql";
-      }
-      switch(provider.ToLower())
-      {
-          case "mssql":
-              services.AddDbContext<AppDbContext>(options =>
-                  options.UseSqlServer(appDbContextConnectionString,
-                  optionsBuilder => optionsBuilder.MigrationsAssembly("CivicSpace.Data")),
-                  ServiceLifetime.Scoped);
-              break;
-          //case "mysql":
-          //    services.AddDbContext<AppDbContext>(options =>
-          //        options.UseMySql(appDbContextConnectionString,
-          //        optionsBuilder => optionsBuilder.MigrationsAssembly("Banico.EntityFrameworkCore")),
-          //        ServiceLifetime.Scoped);
-          //    break;
-          //case "sqlite":
-          //    if (string.IsNullOrEmpty(appDbContextConnectionString))
-          //    {
-          //        var connectionStringBuilder = new Microsoft.Data.Sqlite.SqliteConnectionStringBuilder { DataSource = "banico.db" };
-          //        appDbContextConnectionString = connectionStringBuilder.ToString();
-          //    }
-          //    services.AddDbContext<AppDbContext>(options =>
-          //        options.UseSqlite(appDbContextConnectionString,
-          //        optionsBuilder => optionsBuilder.MigrationsAssembly("Banico.EntityFrameworkCore")),
-          //        ServiceLifetime.Scoped);
-          //    break;
-      }
-
-      services.AddOptions();
-      services.AddScoped<INodeRepository, NodeRepository>();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseCosmos(
+                    configuration.GetConnectionString("CosmosDbConnection") ?? configuration["CosmosDbEndpoint"],
+                    configuration["CosmosDbKey"],
+                    configuration["CosmosDbDatabaseName"]
+                );
+            });
+            services.AddOptions();
+            services.AddScoped<INodeRepository, NodeRepository>();
     }
 
     //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
