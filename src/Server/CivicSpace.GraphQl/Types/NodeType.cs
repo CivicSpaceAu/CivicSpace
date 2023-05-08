4d09724e4cd0f755e3ab84dff1bd13fedb22aa0b
Jason Banico <jason.banico@recordpoint.com>
2023-05-08 13:30:47 +1000

    adding query

---
+++ src/Server/CivicSpace.GraphQl/Types/NodeType.cs
@@ -0,0 +1,22 @@
+﻿using CivicSpace.Core.Content;
+using GraphQL.Types;
+
+namespace CivicSpace.GraphQl.Types
+{
+    public class NodeType : ObjectGraphType<Node>
+    {
+        public NodeType() 
+        {
+            Field(n => n.Id);
+            Field(n => n.Tenant);
+            Field(n => n.Module);
+            Field(n => n.Type);
+            Field(n => n.Title);
+            Field(n => n.Content);
+            Field(n => n.Status);
+            Field(n => n.Slug);
+            Field(n => n.ParentNodeId);
+            Field(n => n.Path);
+        }
+}
+}
